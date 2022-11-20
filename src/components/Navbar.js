daf7767a77ba14422c150d6dff0e6b0cf943369c
Selamet Saputra <keeita768900@gmail.com>
2022-11-20 21:52:11 +0700

    menambahkan komponen pada bagian Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scolled, seScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        seScrolled(true);
+      } else {
+        seScrolled(false);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+  });
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={''} alt="logo" />
+          <img src={''} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           {' '}
@@ -16,16 +30,32 @@ export const Navbar = () => {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>
+              Skills
+            </Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>
+              Projects
+            </Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={''} alt="" />
+              </a>
+              <a href="#">
+                <img src={''} alt="" />
+              </a>
+              <a href="#">
+                <img src={''} alt="" />
+              </a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
